b7610764d126f423677c5ffa385fe889819de6ed
nghitrampham <nghitrampham@gmail.com>
2017-11-16 18:38:01 -0800

    updated

--- reu-17-code/Image_pre_processing.m
+++ reu-17-code/Image_pre_processing.m
@@ -1,40 +1,40 @@
-%
+% test different methods
 
-% I = imread('test_knee.jpg');
-% figure()
-% imshow(I)
+I = imread('test_knee.jpg');
+figure()
+imshow(I)
 
-% %median filter 
-% I0 = medfilt3(I);
-% figure(4)
-% imshow(I0)
-% imwrite(I0, 'med_filter.jpg');
-% 
-% %Gaussian filter 
-% I5 = imgaussfilt3(I);
-% figure(5)
-% imshow(I5)
-% imwrite(I5, 'Gauss_filter.jpg');
-% 
-% back_ground = imopen(I,strel('disk',15));
-% I2 = I- back_ground;
-% figure(1)
-% imshow(I2)
-% imwrite(I2, 'clean_knee.jpg');
+%median filter 
+I0 = medfilt3(I);
+figure(4)
+imshow(I0)
+imwrite(I0, 'med_filter.jpg');
 
-% 
-% %Image constrast 
-% A = double(im2bw(imread('test_knee.jpg')));
-% I3 = imadjust((A));
-% figure(2)
-% imshow(I3)
-% 
-% %Threshold the image
-% bw = imbinarize(A);
-% bw = bwareaopen(bw,10);
-% figure(3)
-% imshow(bw)
-% imwrite(bw, 'bw_knee.jpg');
+%Gaussian filter 
+I5 = imgaussfilt3(I);
+figure(5)
+imshow(I5)
+imwrite(I5, 'Gauss_filter.jpg');
+
+back_ground = imopen(I,strel('disk',15));
+I2 = I- back_ground;
+figure(1)
+imshow(I2)
+imwrite(I2, 'clean_knee.jpg');
+
+
+%Image constrast 
+A = double(im2bw(imread('test_knee.jpg')));
+I3 = imadjust((A));
+figure(2)
+imshow(I3)
+
+%Threshold the image
+bw = imbinarize(A);
+bw = bwareaopen(bw,10);
+figure(3)
+imshow(bw)
+imwrite(bw, 'bw_knee.jpg');
 
 
 % detecting cell
@@ -46,23 +46,7 @@ fudgeFactor = 0.5;
 BWs = edge(I6, 'sobel', threshold*fudgeFactor);
 figure(8), imshow(BWs);
 
-%dilate the image
-se90 = strel('line', 3,90);
-se0 = strel('line', 3,0);
-BWsdil = imdilate(BWs, [se90, se0]);
-figure(9), imshow(BWsdil)
-
 % Fill interior gap 
 BWfill = imfill(BWsdil, 'holes');
 figure(10), imshow(BWfill)
 
-%remove connected objects on border 
-BWnobord = imclearborder(BWfill, 4);
-figure(11), imshow(BWnobord)
-
-%smoothen the object
-seD = strel('diamond',1);
-BWfinal = imerode(BWnobord, seD);
-BWfinal = imerode(BWfinal, seD);
-figure(12)
-imshow(BWfinal)
